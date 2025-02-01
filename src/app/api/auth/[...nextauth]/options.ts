dd24ea3c68075edd1d92e6fed9b68ae8df329c15
Vivek <vy212205@gmail.com>
2025-02-01 11:44:32 +0530

    debug4

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -47,7 +47,7 @@ export const authOptions : NextAuthOptions = {
     callbacks:{
         async jwt({token,user}){
             if(user){
-                token._id = user._id,
+                token._id = user.id,
                 token.isVerified = user.isVerified,
                 token.isAcceptingMessages = user.isAcceptingMessages,
                 token.username = user.username
